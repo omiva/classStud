4dfc9c8d605e5694e544205ef057cece2d6a25b4
Yuvaraj M <yuvm19is@cmrit.ac.in>
2023-02-22 19:05:28 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args)
+    {
+        Scanner ns= new Scanner(System.in);
+        int x=ns.nextInt();
+
+        for(int j=0; j<=3;j++)
+        {
+            Scanner a = new Scanner(System.in);
+            String Usn=a.next();
+
+            Scanner b = new Scanner(System.in);
+            String Name=b.next();
+
+            Scanner c = new Scanner(System.in);
+            String Branch=c.next();
+
+            Scanner d = new Scanner(System.in);
+            String Phone=d.next();
+        }
+    }
+}
